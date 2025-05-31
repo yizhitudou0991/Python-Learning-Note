81f150f692ca580f550a5f429225bceec18ce4f6
yizhitudou0991 <604035243@qq.com>
2025-05-31 21:32:13 +0800

    1

--- 1007ex_sort.py
+++ 1007ex_sort.py
@@ -13,6 +13,11 @@ students = [
 def student_sort_key(student):
     return student[1]
 
-students.sort(key=student_sort_key)
+students.sort(key=student_sort_key,reverse=True) # 按照年龄倒序排列
 
 print(students)
+
+#sorted（）函数
+age_list=[22,35,10,18]
+new_age_list = sorted(age_list,reverse=True) #sorted()函数获取一个新的列表
+print(new_age_list)
