b2a95c7f1d6e4d12f1531eaf40f55a438e6d0bac
yizhitudou0991 <604035243@qq.com>
2025-06-03 18:23:29 +0800

    lambda function

--- 1003ex_lambda.py
+++ 1003ex_lambda.py
@@ -22,3 +22,15 @@ double = times(2) # double = lambda x: x * 2
 
 print(double(2))
 print(double(3))
+
+callables = []
+for i in range(1,4):
+    callables.append(lambda:i)
+for f in callables:
+    print(f()) # 打印的结果为三个3，因为第28行循环变量i在循环结束后仍然存在，并且保留最后一个值，循环结束的时候i是3
+
+callables = []
+for i in range(1,4):
+    callables.append(lambda a = i:a)
+for f in callables:
+    print(f())
