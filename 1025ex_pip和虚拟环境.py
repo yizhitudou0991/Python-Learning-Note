195b259adc0076dd348ec96a024a158cffa43fcc
yizhitudou0991 <604035243@qq.com>
2025-06-04 20:37:14 +0800

    pip和虚拟环境

---
+++ 1025ex_pip和虚拟环境.py
@@ -0,0 +1,11 @@
+# python package索引
+# python本身提供了非常丰富的标准库，但是如果你需要的包不在标准库中，那么你可以从pypi中获得。pypi时最大的python仓库，
+# 时由python社区来维护的。
+
+# package版本
+# pypi中的所有package都有版本号，如果需要获取这些package，最好指定需要的版本号。
+# major.minor.patch
+
+# 虚拟环境
+# 为什么需要虚拟环境：假设多个python的项目代码需要同一个package的不同版本，每个项目没有独立的环境就难以解决这个问题。
+# 虚拟环境就是一个独立的环境（文件夹）用于存放安装的package和必须的python的开发与运行工具
